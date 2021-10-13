a8a8e39102b5740a4ae31f776995ee69aae2f9cd
XNT <famonterrey@uc.cl>
2021-10-13 18:15:38 -0300

    Clase 13.1: Cupones utilizando la opción switch

--- descuentos.js
+++ descuentos.js
@@ -16,9 +16,23 @@ function calcularPrecioConDescuento(precio, descuento) {
 
 function PriceDiscount() {
     const inputPrice = document.getElementById("InputPrice").value;
-    const inputDiscount = document.getElementById("InputDiscount").value;
+    const inputCupon = document.getElementById("InputCupon").value;
 
-    const PrecioConDescuento = calcularPrecioConDescuento(inputPrice, inputDiscount);
+    let descuento;
+
+    switch(inputCupon) {
+        case cupones[0]: //cupon 1
+            descuento = 15;
+        break;
+        case cupones[1]: //cupon 2
+            descuento = 30;
+        break;
+        case cupones[2]: //cupon 3
+            descuento = 50;
+        break;
+    }
+
+    const PrecioConDescuento = calcularPrecioConDescuento(inputPrice, descuento);
     const ResultP = document.getElementById("ResultP");
     ResultP.innerText = "El precio con descuento es: " + PrecioConDescuento;
 }
